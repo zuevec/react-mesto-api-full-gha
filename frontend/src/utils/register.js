bff8e2f841fddcaea32ffcb85d9e976b77fb72cb
Dmitriy Bezrukov <dima1989o-z@yandex.ru>
2023-09-25 14:00:04 +0300

    https

--- frontend/src/utils/register.js
+++ frontend/src/utils/register.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://backend.zuevec.nomoredomainsrocks.ru/'
+const baseUrl = 'https://backend.zuevec.nomoredomainsrocks.ru/'
 
 const handleResponse = async (data) => {
   const res = await data.json();
