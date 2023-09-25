69cca76fea82126572f7e77a63eeeac623da4467
Dmitriy Bezrukov <dima1989o-z@yandex.ru>
2023-09-26 01:23:03 +0300

    12312

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, _, res, next) => {
   const statusCode = err.statusCode || 500;
 
-  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
+  const message = statusCode === 500 ? err.message : err.message;
   res.status(statusCode).send({ message });
   next();
 };
